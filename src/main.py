5e97dcdb01ccd4950dfd048512bb62232e86d57b
tukkimiesjonni <tukkimiesjonni@gmail.com>
2025-04-01 12:39:46 +0300

    Added proper structure so the project is kinda testable now

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
-"""Importing pyfiglet for nice ASCII art"""
-
+"""Importing pyfiglet for nice ASCII art, and other packages for functions"""
+from crypt.encryption import encrypt
+from crypt.decryption import decrypt
+from keys.key_generation import compute_modulus, compute_primes, compute_private_exponent, compute_public_exponent, carmichael_lambda
 import pyfiglet
 
 
@@ -36,6 +38,18 @@ def main():
 
         if selection == "1":
             plaintext_string = input("|| ** Input string to encrypt".ljust(max_length) + " ||\n")
+            bit_length = int(input("|| Input desired bit length".ljust(max_length) + " ||\n"))
+            primes = compute_primes(bit_length)
+            modulus = compute_modulus(primes)
+            carmichael_n = carmichael_lambda(modulus)
+            public_exponent = compute_public_exponent(carmichael_n)
+            private_exponent = compute_private_exponent(public_exponent, carmichael_n)
+
+            print(f"Public Key: (n={carmichael_n}, e={public_exponent})".ljust(max_length) + " ||\n")
+            print(f"Private Key: (n={carmichael_n}, d={private_exponent})".ljust(max_length) + " ||\n")
+            print(f"Plaintext message: {plaintext_string}".ljust(max_length) + " ||\n")
+            encrypted_string = encrypt(plaintext_string, public_exponent, modulus)
+            print(f"Encrypted string: {encrypted_string}")
             # Insert input string here
             # Input key bit length
             # Run encrypting functions
@@ -43,7 +57,10 @@ def main():
             # Print key
             # Print encrypted message
         elif selection == "2":
-            encrypted_string = input("|| ** Input string to decrypt".ljust(max_length) + " ||\n")
+            string_input = int(input("|| ** Input the encrypted integer: ").strip()) # Doesn't work wihtout strip()???
+            decrypted_message = decrypt(string_input, private_exponent, modulus)
+            print(f"Decrypted: {decrypted_message}")
+            # encrypted_string = input("|| ** Input string to decrypt".ljust(max_length) + " ||\n")
             # Insert encrypted string
             # Insert key
             # Run decrypting functions
